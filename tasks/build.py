ddb587f8a16e1457e1b9ce09e03c3991df706e4a
swoutch <38116812+swoutch@users.noreply.github.com>
2023-10-13 22:01:46 +0200

    Clean dist before build

--- tasks/build.py
+++ tasks/build.py
@@ -1,7 +1,18 @@
+import shutil
 import sys
+from pathlib import Path
 
 import build.__main__
 
 
 if __name__ == '__main__':
+    dist_path = Path("dist")
+
+    if dist_path.exists() and dist_path.is_dir():
+        for item in dist_path.iterdir():
+            if item.is_file():
+                item.unlink()
+            elif item.is_dir():
+                shutil.rmtree(item)
+
     build.__main__.main(sys.argv[1:], 'python -m tasks.build')
